1b8853d6856084aa825ff9ec7c6e66a9b5a513f9
Kim Pearton <kim.pearton@live.co.uk>
2019-04-16 12:37:14 +0000

    Create home app

---
+++ home/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth, messages
+from django.contrib.auth.models import User
+
+# Create your views here.
