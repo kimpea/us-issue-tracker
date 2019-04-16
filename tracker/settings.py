af0a79568a1308374be224660a465cbb3679662f
Kim Pearton <kim.pearton@live.co.uk>
2019-04-16 12:54:14 +0000

    Add home to installed apps and static directory

--- tracker/settings.py
+++ tracker/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_forms_bootstrap',
-    'accounts'
+    'accounts',
+    'home'
 ]
 
 MIDDLEWARE = [
@@ -125,5 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
