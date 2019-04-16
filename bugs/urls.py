1accaf62f4cf26001c00e15a45d5e8bf50cf579a
Kim Pearton <kim.pearton@live.co.uk>
2019-04-16 15:03:38 +0000

    Add link for upvote bug

--- bugs/urls.py
+++ bugs/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
-from .views import bugs, bug_detail, report_bug
+from .views import bugs, bug_detail, report_bug, upvote_bug
 
 urlpatterns = [
     url(r'^$', bugs, name="bugs"),
     url(r'^bug_detail/(?P<id>\d+)/$', bug_detail, name="bug_detail"),
+    url(r'^(?P<id>\d+)/upvote_bug/$', upvote_bug, name='upvote_bug'),
     url(r'^report_bug/$', report_bug, name='report_bug'),
 ]
