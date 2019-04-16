47ddc131805d3dfbc975c3b47f13df5f85b134ac
Kim Pearton <kim.pearton@live.co.uk>
2019-04-16 14:04:23 +0000

    Add media url

--- tracker/urls.py
+++ tracker/urls.py
@@ -17,8 +17,12 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from home.views import index
 from accounts import urls as urls_accounts
+from django.views import static
+from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name="index"),
+    url(r'^accounts/', include(urls_accounts)),
+    url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT})
 ]
