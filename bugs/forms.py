e40a678d6d7446337d41a0ce6b2d8b6697f1e049
Kim Pearton <kim.pearton@live.co.uk>
2019-04-16 15:13:58 +0000

    Add comment form

--- bugs/forms.py
+++ bugs/forms.py
@@ -1,7 +1,15 @@
 from django import forms
-from .models import Bug
+from .models import Bug, BugComments
 
 class ReportBugForm(forms.ModelForm):
     class Meta: 
         model = Bug
         fields = ('name', 'description')
+
+class BugCommentForm(forms.ModelForm):
+    comment = forms.CharField(widget=forms.Textarea(
+        attrs={'rows': '4', 'cols': '5'}))
+
+    class Meta:
+        model = BugComments
+        fields = ['comment']
