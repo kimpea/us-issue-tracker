63fbb10f61b89a1857debe2c3e8ad1049043ee6b
Kim Pearton <kim.pearton@live.co.uk>
2019-04-16 14:27:34 +0000

    Add bug links

--- tracker/urls.py
+++ tracker/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from home.views import index
 from accounts import urls as urls_accounts
+from bugs import urls as urls_bugs
 from django.views import static
 from .settings import MEDIA_ROOT
 
@@ -24,5 +25,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name="index"),
     url(r'^accounts/', include(urls_accounts)),
+    url(r'^bugs/', include(urls_bugs)),
     url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT})
 ]
