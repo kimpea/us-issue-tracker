7d29a9947de4ac134768d7c027544a8ec2388cef
Kim Pearton <kim.pearton@live.co.uk>
2019-04-16 14:43:01 +0000

    Add views for bug detail and report bug

--- bugs/views.py
+++ bugs/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Bug
+from .forms import ReportBugForm
 import datetime
 
 # Create your views here.
@@ -26,3 +27,33 @@ def bugs(request):
         bugs = paginator.page(paginator.num_pages)
         
     return render(request, "bugs.html", {"bugs": bugs})
+
+def bug_detail(request, id):
+    """
+    A view which displays details for a specific bug
+    """
+    bug = get_object_or_404(Bug, id=id)
+    upvotes = bug.upvotes
+        
+    return render(request, "bug_detail.html", {
+        'bug': bug,
+        'upvotes': upvotes
+    })
+    
+@login_required
+def report_bug(request, id=None):
+    """
+    A view which renders page for report_bug form. 
+    User must be logged in to access this page. 
+    """
+    bug = get_object_or_404(Bug, id=id) if id else None
+    if request.method == "POST":
+        form = ReportBugForm(request.POST, request.FILES, instance=bug)
+        if form.is_valid():
+            bug = form.save(commit=False)
+            bug.user = request.user
+            bug.save()
+            return redirect(bug_detail, bug.id)
+    else:
+        form = ReportBugForm(instance=bug)
+    return render(request, 'report_bug.html', {'form': form})
