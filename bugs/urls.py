cd41588d2d012c9e51fe050550ff1d53f2a5015f
Kim Pearton <kim.pearton@live.co.uk>
2019-04-16 14:43:57 +0000

    Add links for bugs

--- bugs/urls.py
+++ bugs/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
-from .views import bugs
+from .views import bugs, bug_detail, report_bug
 
 urlpatterns = [
-    url(r'^$', bugs, name="bugs")
+    url(r'^$', bugs, name="bugs"),
+    url(r'^bug_detail/(?P<id>\d+)/$', bug_detail, name="bug_detail"),
+    url(r'^report_bug/$', report_bug, name='report_bug'),
 ]
