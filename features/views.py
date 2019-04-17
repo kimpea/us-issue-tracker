35a07eb6d541e6a1848a405c17361a30193da6b7
Kim Pearton <kim.pearton@live.co.uk>
2019-04-17 11:33:45 +0000

    Create features view with paginator

--- features/views.py
+++ features/views.py
@@ -9,3 +9,21 @@ from .forms import RequestFeatureForm
 import datetime
 
 # Create your views here.
+def features(request):
+    """
+    A view which displays all requested features in one page,
+    and also displays graphs 
+    """
+    # Order features by amount of upvotes
+    features_list = Feature.objects.all().order_by('-upvotes')
+    
+    # Pagination for features
+    page = request.GET.get('page', 1)
+    paginator = Paginator(features_list, 10)
+    try:
+        features = paginator.page(page)
+    except PageNotAnInteger:
+        features = paginator.page(1)
+    except EmptyPage:
+        features = paginator.page(paginator.num_pages)
+    return render(request, "features.html", {"features": features})
