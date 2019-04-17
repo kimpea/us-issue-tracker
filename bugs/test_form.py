d5c170a12f85332f14b17e3daa7100bff377da72
Kim Pearton <kim.pearton@live.co.uk>
2019-04-17 12:12:56 +0000

    Create test for report bug form - bug has name and description

--- bugs/test_form.py
+++ bugs/test_form.py
@@ -1,1 +1,10 @@
 from django.test import TestCase
+from .forms import ReportBugForm
+
+class TestReportBugForm(TestCase):
+
+    def test_bug_has_name_and_description(self):
+        form = ReportBugForm(
+            {'name': 'Bug Name',
+            'description': 'Bug Description'})
+        self.assertTrue(form.is_valid())
