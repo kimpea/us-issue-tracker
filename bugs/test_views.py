ce3d457d26fc32aebb0860c0f127d6aed6df1c95
Kim Pearton <kim.pearton@live.co.uk>
2019-04-17 12:22:05 +0000

    Create setup

--- bugs/test_views.py
+++ bugs/test_views.py
@@ -5,3 +5,18 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from bugs.models import Bug, BugComments
 from django.utils import timezone
+
+class TestBugViews(TestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create(username='Test',
+                                        password='testing456')
+        self.user.set_password('testing456')
+        self.user.save()
+        self.client = Client()
+        self.client.login(username='Test',
+                        password='testing456')
+        self.bug = Bug.objects.create(name='Bug Name',
+                                            description='Bug Description',
+                                            user=self.user)
+        self.bug.save()
