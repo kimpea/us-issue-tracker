2e08661cb9ebce5429a72f540a770744e82e02f3
Kim Pearton <kim.pearton@live.co.uk>
2019-04-17 12:14:16 +0000

    Test report bug form - without name or description

--- bugs/test_form.py
+++ bugs/test_form.py
@@ -8,3 +8,11 @@ class TestReportBugForm(TestCase):
             {'name': 'Bug Name',
             'description': 'Bug Description'})
         self.assertTrue(form.is_valid())
+        
+    def test_bug_without_name_or_description(self):
+        form = ReportBugForm({
+            'name': 'Bug Name',
+            'description': ''
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['description'], ['This field is required.'])
