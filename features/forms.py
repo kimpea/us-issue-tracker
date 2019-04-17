1e50d0b8f6454c8e4cdcecf3f10b0d7f13d51275
Kim Pearton <kim.pearton@live.co.uk>
2019-04-17 13:31:05 +0000

    Create comment form

--- features/forms.py
+++ features/forms.py
@@ -1,7 +1,15 @@
 from django import forms
-from .models import Feature
+from .models import Feature, FeatureComments
 
 class RequestFeatureForm(forms.ModelForm):
     class Meta: 
         model = Feature
         fields = ('name', 'description')
+        
+class FeatureCommentForm(forms.ModelForm):
+    comment = forms.CharField(widget=forms.Textarea(
+        attrs={'rows': '4', 'cols': '5'}))
+
+    class Meta:
+        model = FeatureComments
+        fields = ['comment']
