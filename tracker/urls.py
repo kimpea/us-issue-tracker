0f45389ff6828a46c38deead70844e39ce2980ae
Kim Pearton <kim.pearton@live.co.uk>
2019-04-17 14:41:33 +0000

    Add cart urls to settings

--- tracker/urls.py
+++ tracker/urls.py
@@ -19,6 +19,7 @@ from home.views import index
 from accounts import urls as urls_accounts
 from bugs import urls as urls_bugs
 from features import urls as urls_features
+from cart import urls as urls_cart
 from django.views import static
 from .settings import MEDIA_ROOT
 
@@ -28,5 +29,6 @@ urlpatterns = [
     url(r'^accounts/', include(urls_accounts)),
     url(r'^bugs/', include(urls_bugs)),
     url(r'^features/', include(urls_features)),
+    url(r'^cart/', include(urls_cart)),
     url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT})
 ]
