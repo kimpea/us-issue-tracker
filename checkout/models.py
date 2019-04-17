fbe9383566e214e182d278051d9d160da577a31b
Kim Pearton <kim.pearton@live.co.uk>
2019-04-17 14:55:21 +0000

    Use models from ecommerce app

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from features.models import Feature
 
 # Create your models here.
+
+class Order(models.Model):
+    full_name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(max_length=100, blank=False, default='example@example.com')
+    
+    def __str__(self):
+        return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
+        
+        
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False)
+    feature = models.ForeignKey(Feature, null=False, default="Feature")
+    quantity = models.IntegerField(blank=False)
+    
+    def __str__(self):
+        return "{0} {1} @ {2}".format(self.quantity, self.feature.name, self.feature.price)
