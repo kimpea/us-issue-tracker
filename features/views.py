0d1dd92ce7c6751166eae7e1296357d63ea887a7
Kim Pearton <kim.pearton@live.co.uk>
2019-04-17 11:32:20 +0000

    Set up views for features

--- features/views.py
+++ features/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Feature
+from .forms import RequestFeatureForm
+import datetime
 
 # Create your views here.
