e90f51f12262406bb5d5a7679f48e80dd07cd8da
Kim Pearton <kim.pearton@live.co.uk>
2019-04-17 13:50:07 +0000

    Create urls for features

--- features/urls.py
+++ features/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url, include
-from .views import features
+from .views import features, feature_detail, request_feature, feature_comment
 
 urlpatterns = [
-    url(r'^$', features, name="features")
+    url(r'^$', features, name="features"),
+    url(r'^feature_detail/(?P<id>\d+)/$', feature_detail, name="feature_detail"),
+    url(r'^request_feature/$', request_feature, name='request_feature'),
+    url(r'^feature_comment/(?P<id>\d+)/$', feature_comment, name='feature_comment'),
 ]
