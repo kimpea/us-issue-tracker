7a16c12180243d74503ef7c1f4be051f4de0f138
Kim Pearton <kim.pearton@live.co.uk>
2019-04-17 12:04:23 +0000

    Create test for report_bug function

--- bugs/test_models.py
+++ bugs/test_models.py
@@ -4,11 +4,17 @@ from django.contrib.auth.models import User
 
 # Create your tests here.
 class TestBug(TestCase):
-    """
-    Here we'll define the tests that we'll run against our
-    Bug model
-    """
-
     def test_str(self):
         test_name = Bug(name='A bug')
         self.assertEqual(str(test_name), 'A bug')
+        
+    def test_report_bug(self):
+            user = User()
+            user.save()
+            bug = Bug(name="Bug", description="Bug Description", user=user)
+            bug.save()
+            self.assertEqual(bug.name, "Bug")
+            self.assertEqual(bug.description, "Bug Description")
+            self.assertEqual(bug.user, user)
+            self.assertEqual(bug.status, "OPEN")
+            self.assertEqual(bug.upvotes, 0)
