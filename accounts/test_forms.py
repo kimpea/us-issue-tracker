22796f8cdda8427989706446026be7f3daa4b8ee
Kim Pearton <kim.pearton@live.co.uk>
2019-04-18 07:33:52 +0000

    Test if user can be registered

--- accounts/test_forms.py
+++ accounts/test_forms.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 from .forms import UserLoginForm, UserRegistrationForm
 from django.contrib.auth.models import User
+
+# Create your tests here.
+class TestForms(TestCase):
+    
+    # Registration Form Tests
+    def test_if_user_can_be_registered(self):
+        form = UserRegistrationForm(
+            {
+                'email': 'test@test.com',
+                'username': 'Testing',
+                'password1': 'test123',
+                'password2': 'test123',
+            }
+        )
+        self.assertTrue(form.is_valid())
