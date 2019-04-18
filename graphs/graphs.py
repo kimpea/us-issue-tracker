1e590ce9a56ea088daec772be3c9bc0d265fd27f
Kim Pearton <kim.pearton@live.co.uk>
2019-04-18 12:25:50 +0000

    Set custom styles for graphs

--- graphs/graphs.py
+++ graphs/graphs.py
@@ -3,3 +3,21 @@ from bugs.models import Bug
 from features.models import Feature
 from pygal.style import Style
 from datetime import date, datetime, timedelta
+
+################ Custom Styles ################
+
+custom_style = Style(
+    background='transparent',
+    plot_background='transparent',
+    foreground='#000',
+    foreground_strong='#fafafa',
+    foreground_subtle='#fafafa',
+    opacity='.9',
+    opacity_hover='1',
+    colors=('#00171f', '#007ea7', '#003459'),
+
+    value_font_size=30,
+    legend_font_size=20,
+    tooltip_font_size=30,
+    no_data_font_size=30
+)
