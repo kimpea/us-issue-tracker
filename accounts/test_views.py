d4dd925b2d40d55726d2ce149303f2b30171cc25
Kim Pearton <kim.pearton@live.co.uk>
2019-04-18 07:29:59 +0000

    Test logout page

--- accounts/test_views.py
+++ accounts/test_views.py
@@ -38,3 +38,16 @@ class TestViews(TestCase):
     def test_login_page_when_logged_out(self):
         page = self.client.get("/accounts/login/")
         self.assertEqual(page.status_code, 200)
+        
+    # Logout Page
+    def test_logout_page_when_logged_in(self):
+        user = User.objects.create_user(username='Testing', password='test123')
+        user.save()
+        self.client.login(username='Testing', password='test123')
+        response = self.client.get("/accounts/logout/",follow=True)
+        for i in response.context['messages']:
+            message = str(i)
+        
+        self.assertEqual(message,'You have successfully been logged out!')
+        self.assertRedirects(response, expected_url=reverse('index'),
+        status_code=302, target_status_code=200, fetch_redirect_response=True)
