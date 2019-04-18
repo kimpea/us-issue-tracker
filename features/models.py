ddb50bf666149838c8ca7aa78c267bcd92aafb2e
Kim Pearton <kim.pearton@live.co.uk>
2019-04-18 13:02:47 +0000

    Add graph fields to model

--- features/models.py
+++ features/models.py
@@ -13,6 +13,10 @@ class Feature(models.Model):
     description = models.TextField(blank=False)
     user = models.ForeignKey(User, default=None)
     date_created = models.DateTimeField(auto_now_add=True)
+    incomplete_date = models.DateTimeField(blank=True, default=None, null=True)
+    in_progress_date = models.DateTimeField(
+        blank=True, default=None, null=True)
+    completed_date = models.DateTimeField(blank=True, default=None, null=True)
     upvotes = models.IntegerField(default=0)
     status = models.CharField(choices=STATUS_CHOICES, default='INCOMPLETE', max_length=20)
     price = models.DecimalField(max_digits=6, decimal_places=2, default=20)
