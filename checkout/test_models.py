65f93ed6639d6757323f36fd750e4a580bd150f7
Kim Pearton <kim.pearton@live.co.uk>
2019-04-18 10:11:47 +0000

    Test create order with details

--- checkout/test_models.py
+++ checkout/test_models.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 from .models import Order
 from datetime import datetime
+
+class TestOrderModel(TestCase):
+    def test_create_order_with_details(self):
+        order = Order(full_name="Test Testing", email='test@test.com')
+        order.save()
+        self.assertEquals(order.full_name, "Test Testing")
+        self.assertEquals(order.email, "test@test.com")
