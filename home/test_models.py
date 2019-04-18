ab75af8707b423c7e8982fdb62e6ecbd4bbfd4fe
Kim Pearton <kim.pearton@live.co.uk>
2019-04-18 08:36:23 +0000

    Test Question model

--- home/test_models.py
+++ home/test_models.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 from .models import Question
 from django.contrib.auth.models import User
+
+# Create your tests here.
+class TestQuestion(TestCase):
+    """
+    Here we'll define the tests that we'll run against our
+    Feature model
+    """
+
+    def test_str(self):
+        test_name = Question(question='A question')
+        self.assertEqual(str(test_name), 'A question')
+        
+    def test_ask_question(self):
+        user = User()
+        user.save()
+        question = Question(question="Question", description="Question Description", user=user)
+        question.save()
+        self.assertEqual(question.question, "Question")
+        self.assertEqual(question.description, "Question Description")
