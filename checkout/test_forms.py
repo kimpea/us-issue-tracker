1323064f66645f19248b07af6b7c66bf35ae81d2
Kim Pearton <kim.pearton@live.co.uk>
2019-04-18 10:20:51 +0000

    Test making payment with required values

--- checkout/test_forms.py
+++ checkout/test_forms.py
@@ -1,2 +1,14 @@
 from django.test import TestCase
 from .forms import MakePaymentForm, OrderForm
+
+class TestMakePaymentForm(TestCase):
+    
+    def test_can_make_a_payment_with_required_values(self):
+        form = MakePaymentForm({
+                    'credit_card_number': '4242424242424242', 
+                    'cvv': "111",
+                    'expiry_month': 1,
+                    'expiry_year': 2020,
+                    'stripe_id': 'l'
+        })
+        self.assertTrue(form.is_valid())
