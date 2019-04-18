fc48409d55d47b127d3fecb094e35213d4a27e39
Kim Pearton <kim.pearton@live.co.uk>
2019-04-18 10:27:21 +0000

    Test making payment with email and full name

--- checkout/test_forms.py
+++ checkout/test_forms.py
@@ -22,3 +22,13 @@ class TestMakePaymentForm(TestCase):
                     'expiry_year': 2020,
         })
         self.assertFalse(form.is_valid())
+        
+class TestOrderForm(TestCase):
+    
+    def test_can_make_a_payment_with_required_values(self):
+        form = OrderForm({
+                    'full_name': "Test Testing", 
+                    'email': 'test@test.com'
+                    
+        })
+        self.assertTrue(form.is_valid())
