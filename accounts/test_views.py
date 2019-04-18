3b80b704a19c09b6c364cf76f49f7feca53acbbd
Kim Pearton <kim.pearton@live.co.uk>
2019-04-18 07:02:46 +0000

    Test the home page

--- accounts/test_views.py
+++ accounts/test_views.py
@@ -3,3 +3,11 @@ from django.contrib.auth.models import User
 from .views import index, login, logout, register
 from django.test.client import Client
 from django.core.urlresolvers import reverse
+
+class TestViews(TestCase):
+    
+    # Home Page
+    def test_get_home_page(self):
+        page = self.client.get("/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "index.html")
