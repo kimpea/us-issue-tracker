22bee08c2713308e741ff2804262e3390cf14b91
Kim Pearton <kim.pearton@live.co.uk>
2019-04-18 10:55:47 +0000

    Test index page

--- home/test_views.py
+++ home/test_views.py
@@ -5,3 +5,13 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from home.models import Question
 from django.utils import timezone
+
+class TestHomeViews(TestCase):
+    
+    def test_get_index_page(self):
+        """
+        Testing index view which is the first page user sees
+        """
+        page=self.client.get("/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "index.html")
