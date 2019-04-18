846ebe63efb278462a9515d474c2a6acdfde4059
Kim Pearton <kim.pearton@live.co.uk>
2019-04-18 09:47:05 +0000

    Test that feature has name and description

--- features/test_forms.py
+++ features/test_forms.py
@@ -1,2 +1,10 @@
 from django.test import TestCase
 from .forms import RequestFeatureForm, FeatureCommentForm
+
+class TestRequestFeatureForm(TestCase):
+
+    def test_feature_has_name_and_description(self):
+        form = RequestFeatureForm(
+            {'name': 'Feature Name',
+            'description': 'Feature Description'})
+        self.assertTrue(form.is_valid())
