e8a093bfcc686b9b03d817cb0916d883f7e8a4ac
Kim Pearton <kim.pearton@live.co.uk>
2019-04-18 07:20:15 +0000

    Test login page when user logged in

--- accounts/test_views.py
+++ accounts/test_views.py
@@ -24,3 +24,13 @@ class TestViews(TestCase):
         self.client.login(username='Testing', password='test123')
         page = self.client.get("/accounts/register/")
         self.assertEqual(page.status_code, 302)
+        
+    # Login Page
+    def test_login_page_when_logged_in(self):
+        user = User.objects.create_user(username='Testing',
+        password='test123')
+        user.save()
+        self.client.login(username='Testing', password='test123')
+        response = self.client.get("/accounts/login/", follow=True)
+        self.assertRedirects(response, expected_url=reverse('index'),
+        status_code=302, target_status_code=200, fetch_redirect_response=True)
