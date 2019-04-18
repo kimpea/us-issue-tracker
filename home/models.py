2b6a10a51e6521b63b05bf9f2598adca5f85129d
Kim Pearton <kim.pearton@live.co.uk>
2019-04-18 08:27:41 +0000

    Create Question model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Question(models.Model):
+    question = models.CharField(max_length=254, default='')
+    description = models.TextField(blank=True)
+    user = models.ForeignKey(User, default=None)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question
