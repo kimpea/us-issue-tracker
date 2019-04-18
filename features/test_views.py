2333c6971e0dcbd381f479f0ade1f8af60c66501
Kim Pearton <kim.pearton@live.co.uk>
2019-04-18 09:53:43 +0000

    Set up user in test file

--- features/test_views.py
+++ features/test_views.py
@@ -5,3 +5,18 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from features.models import Feature, FeatureComments
 from django.utils import timezone
+
+class TestFeatureViews(TestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create(username='Test',
+                                        password='testing456')
+        self.user.set_password('testing456')
+        self.user.save()
+        self.client = Client()
+        self.client.login(username='Test',
+                        password='testing456')
+        self.feature = Feature.objects.create(name='Feature Name',
+                                            description='Feature Description',
+                                            user=self.user)
+        self.feature.save()
