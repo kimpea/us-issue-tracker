d98131e5e062b620b9b67be99aa2ba43bc10f034
Kim Pearton <kim.pearton@live.co.uk>
2019-04-18 09:17:15 +0000

    Test feature

--- features/test_models.py
+++ features/test_models.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 from .models import Feature, FeatureComments
 from django.contrib.auth.models import User
+
+# Create your tests here.
+class TestFeature(TestCase):
+    """
+    Here we'll define the tests that we'll run against our
+    Feature model
+    """
+
+    def test_str(self):
+        test_name = Feature(name='A feature')
+        self.assertEqual(str(test_name), 'A feature')
