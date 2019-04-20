b9859eb2a13598ab30d4cd9cd9fdac684de14947
Kim Pearton <kim.pearton@live.co.uk>
2019-04-20 15:05:58 +0000

    Remove media urls

--- tracker/urls.py
+++ tracker/urls.py
@@ -22,7 +22,6 @@ from features import urls as urls_features
 from cart import urls as urls_cart
 from checkout import urls as urls_checkout
 from django.views import static
-from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -33,6 +32,5 @@ urlpatterns = [
     url(r'^bugs/', include(urls_bugs)),
     url(r'^features/', include(urls_features)),
     url(r'^cart/', include(urls_cart)),
-    url(r'^checkout/', include(urls_checkout)),
-    url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT})
+    url(r'^checkout/', include(urls_checkout))
 ]
